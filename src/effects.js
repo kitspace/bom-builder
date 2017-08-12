2a36ff08d0985a00efd947e96bef4c81c5c5df49
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-08-12 17:47:31 +0100

    Start thinking about findSuggestions effect

--- src/effects.js
+++ src/effects.js
@@ -1,9 +1,22 @@
+const immutableDiff = require('immutable-diff').default
 const {
   initialState,
   changed,
 } = require('./state')
 
-function effects(state, actions) {
+function effects(diff, state) {
+  diff.forEach(d => {
+    const op = d.get('op')
+    const path = d.get('path')
+    const value = d.get('value')
+    if (path.size === 2 && op === 'add' && path.get(0) === 'lines') {
+      findSuggestions(value)
+    } else if (path.size === 6 && path.includes('partNumbers')
+      && path.includes('selected') && typeof path.get(3) === 'number') {
+      const line = state.data.present.getIn(path.slice(0, 4))
+      findSuggestions(line)
+    }
+  })
 }
 
 function subscribeEffects(store, actions) {
@@ -11,10 +24,13 @@ function subscribeEffects(store, actions) {
   store.subscribe(() => {
     const state = store.getState()
     if (changed(prev_state, state)) {
-      effects(state, actions)
+      effects(immutableDiff(prev_state.data.present, state.data.present), state)
       prev_state = state
     }
   })
+}
+
+function findSuggestions(line) {
 }
 
 module.exports = {subscribeEffects}

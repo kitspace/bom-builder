2066080d7c34b6af8885218a5e84f6110fa696fb
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-08-13 19:52:22 +0100

    Remove needsSuggestion check in findSuggestions

--- src/effects.js
+++ src/effects.js
@@ -35,34 +35,30 @@ function findSuggestions(line, actions) {
   const partNumbers = line.get('partNumbers')
   const id = line.get('id')
 
-  const needsSuggestions = partNumbers.some(p => {
-    return p.get('part') === ''
+  const suggestionSkus = line.get('suggestions').flatMap(s => {
+    return s.get('offers').map(offer => offer.get('sku'))
   })
 
-  if (needsSuggestions) {
-    const suggestionSkus = line.get('suggestions').flatMap(s => {
-      return s.get('offers').map(offer => offer.get('sku'))
+  const skus = line.get('retailers').entrySeq().map(([vendor, part]) => {
+    if (vendor === 'Digikey') {
+      vendor = 'Digi-Key'
+    }
+    if (part !== '') {
+      return {vendor, part}
+    }
+    return null
+  })
+    .filter(x => x != null)
+    .filter(part => !suggestionSkus.includes(part))
+
+  const ps = skus.map(sku => {
+    return getPartinfo(sku)
+  }).map(p => {
+    return p.then(part => {
+      return actions.addSuggestion({id, part})
     })
-    const skus = line.get('retailers').entrySeq().map(([vendor, part]) => {
-      if (vendor === 'Digikey') {
-        vendor = 'Digi-Key'
-      }
-      if (part !== '') {
-        return {vendor, part}
-      }
-      return null
-    })
-      .filter(x => x != null)
-      .filter(part => !suggestionSkus.includes(part))
-    const ps = skus.map(sku => {
-      return getPartinfo(sku)
-    }).map(p => {
-      return p.then(part => {
-        return actions.addSuggestion({id, part})
-      })
-    })
-    return Promise.all(ps)
-  }
+  })
+  return Promise.all(ps)
 }
 
 module.exports = {subscribeEffects}

dcd0aa615ef6b9c084c2498b6a97ba90efd64423
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-11-22 22:43:15 +0000

    Upgrade react

--- src/index.js
+++ src/index.js
@@ -4,8 +4,5 @@ import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-import Perf from 'react-addons-perf'
-window.Perf = Perf
-
 render(<App editable={true} />, document.getElementById('root'))
 registerServiceWorker()

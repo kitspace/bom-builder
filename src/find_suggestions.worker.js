d1ab69216c9f373bf86cc4d88de7776bcf6dad04
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2019-03-09 13:27:14 +0000

    Make find_suggestions worker not break production build
    https://github.com/timarney/react-app-rewired/issues/362

--- src/find_suggestions.worker.js
+++ src/find_suggestions.worker.js
@@ -4,17 +4,17 @@ import {fromJS} from 'immutable'
 import {findSuggestions} from './find_suggestions'
 import {actions as unboundActions} from './state'
 
-/* global self */
-
 const dispatch = action => {
   if (action.value.suggestions) {
     action.value.suggestions = action.value.suggestions.toJS()
   }
+  // eslint-disable-next-line no-restricted-globals
   self.postMessage(action)
 }
 
 const actions = bindActionCreators(unboundActions, dispatch.bind(this))
 
+// eslint-disable-next-line no-restricted-globals
 self.addEventListener('message', event => {
   const {lineId, line, suggestions} = event.data
   findSuggestions(lineId, fromJS(line), fromJS(suggestions), actions)

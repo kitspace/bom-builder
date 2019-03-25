3cf8ea64ffad0685d398d66aa1df583f748a9b09
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2019-03-25 22:32:24 +0000

    Remove react-window

--- src/body.js
+++ src/body.js
@@ -1,36 +1,15 @@
 import './buy_parts.css'
 import React from 'react'
 import * as reactRedux from 'react-redux'
-import {FixedSizeList} from 'react-window'
-import AutoSizer from 'react-virtualized-auto-sizer'
 
 import Line from './line'
-
-class Row extends React.PureComponent {
-  render() {
-    const props = this.props
-    const id = props.data.get(props.index)
-    return <Line className="bomLine" key={id} lineId={id} style={props.style} />
-  }
-}
 
 function Body(props) {
   return (
     <div style={{flex: '1 1 auto'}}>
-      <AutoSizer>
-        {({width, height}) => (
-          <FixedSizeList
-            className="tableBody"
-            height={height}
-            width={width}
-            itemCount={props.lineIds.size}
-            itemSize={29}
-            itemData={props.lineIds}
-          >
-            {Row}
-          </FixedSizeList>
-        )}
-      </AutoSizer>
+      {props.lineIds.map(id => (
+        <Line className="bomLine" key={id} lineId={id} style={props.style} />
+      ))}
     </div>
   )
 }

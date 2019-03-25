76256c44f8e5f23102b57850b368186dd9894c43
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2019-03-25 21:52:15 +0000

    Remove the service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react'
 import {render} from 'react-dom'
 import './index.css'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+import {unregister} from './registerServiceWorker'
+
+unregister()
 
 render(<App editable={true} />, document.getElementById('root'))
-registerServiceWorker()

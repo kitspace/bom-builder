bcda50888011afe40912f51d8387d9ba99cb64ff
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2019-06-29 23:15:14 +0100

    Clear out data-grid and play with spreadsheet-grid

--- src/table.js
+++ src/table.js
@@ -3,7 +3,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import * as redux from 'redux'
 import * as reactRedux from 'react-redux'
-import ReactDataGrid from 'react-data-grid'
 import * as immutable from 'immutable'
 import * as semantic from 'semantic-ui-react'
 import AutoSizer from 'react-virtualized-auto-sizer'
@@ -11,150 +10,104 @@ import {Grid, Input, Select} from 'react-spreadsheet-grid'
 
 import {actions} from './state'
 
-function mpnFormatter({value}) {
-  value = value || immutable.Map()
-  return (
-    <>
-      <div>{value.get('manufacturer')}</div>
-      <div>{value.get('part')}</div>
-    </>
-  )
+const retailerRender = retailer => (row, {active, focus}) => {
+  const value = row.getIn(['retailers', retailer])
+  if (active) {
+    return <Input value={value} focus={focus} />
+  }
+  if (value) {
+    return <semantic.Icon name="check" color="green" />
+  }
+  return <div />
 }
-
-class MpnEditor extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {mpn: props.value}
-  }
-  getValue() {
-    // an object of key/value pairs to be merged back to the row
-    return {[this.props.column.key]: this.state.mpn || this.props.value}
-  }
-
-  getInputNode() {
-    return ReactDOM.findDOMNode(this).getElementsByClassName('partInput')[0]
-  }
-  disableContainerStyles() {
-    return true
-  }
-
-  handlePartChange = event => {
-    const value = event.target.value
-    this.setState(state => {
-      return {mpn: state.mpn.set('part', value)}
-    })
-  }
-
-  handleManufacturerChange = event => {
-    const value = event.target.value
-    this.setState(state => {
-      return {mpn: state.mpn.set('manufacturer', value)}
-    })
-  }
-
-  render() {
-    const mpn = this.state.mpn || this.props.value
-    return (
-      <div style={{background: 'white'}}>
-        <input
-          onChange={this.handleManufacturerChange}
-          value={mpn.get('manufacturer')}
-        />
-        <input
-          className="partInput"
-          onChange={this.handlePartChange}
-          value={mpn.get('part')}
-        />
-      </div>
-    )
-  }
-}
-
-const columns = [
-  {
-    id: 'reference',
-    title: () => 'References',
-    value: (row, {focus}) => {
-      return <Input value={row.get('reference')} focus={focus} />
-    }
-  },
-  {
-    id: 'quantity',
-    title: () => 'Qty',
-    value: (row, {focus}) => {
-      return <Input value={row.get('quantity')} focus={focus} />
-    }
-  },
-  {
-    id: 'description',
-    title: () => 'Description',
-    value: (row, {focus}) => {
-      return <Input value={row.get('description')} focus={focus} />
-    }
-  },
-  {
-    title: () => 'Part Number',
-    value: (row, {focus}) => {
-      const mpn = row.getIn(['partNumbers', 0])
-      return (
-        <div>
-          <Input value={mpn.get('manufacturer')} />
-          <Input value={mpn.get('part')} focus={focus} />
-        </div>
-      )
-    }
-  },
-  {
-    title: () => 'Digikey',
-    value: (row, {focus}) => {
-      const value = row.getIn(['retailers', 'Digikey'])
-      if (focus) {
-        return <Input value={value} focus={focus} />
-      }
-      if (value) {
-        return <semantic.Icon name="check" color="green" />
-      }
-      return null
-    }
-  },
-  {
-    title: () => 'Mouser',
-    value: (row, {focus}) => {
-      return <Input value={row.getIn(['retailers', 'Mouser'])} focus={focus} />
-    }
-  },
-  {
-    title: () => 'RS',
-    value: (row, {focus}) => {
-      return <Input value={row.getIn(['retailers', 'RS'])} focus={focus} />
-    }
-  },
-  {
-    title: () => 'Farnell',
-    value: (row, {focus}) => {
-      return <Input value={row.getIn(['retailers', 'Farnell'])} focus={focus} />
-    }
-  }
-]
 
 class Table extends React.Component {
-  onGridRowsUpdated = ({fromRow, toRow, updated}) => {
-    for (let row = fromRow; row <= toRow; row++) {
-      const line = this.props.lines.get(row)
-      const lineId = line.get('id')
-      Object.keys(updated).forEach(k => {
-        const field = immutable.List(k.split(':'))
-        const value = updated[k]
-        this.props.setField({lineId, field, value})
-      })
-    }
+  state = {
+    columnWidths: {
+      quantity: 7,
+      'retailers:Digikey': 7,
+      'retailers:Mouser': 7,
+      'retailers:RS': 7,
+      'retailers:Farnell': 7,
+    },
   }
   render() {
+    const columns = [
+      {
+        id: 'reference',
+        title: 'References',
+        value: (row, {focus}) => {
+          return <Input value={row.get('reference')} focus={focus} />
+        },
+      },
+      {
+        id: 'quantity',
+        title: 'Qty',
+        value: (row, {focus}) => {
+          return <Input value={row.get('quantity')} focus={focus} />
+        },
+      },
+      {
+        id: 'description',
+        title: 'Description',
+        value: (row, {focus}) => {
+          return <Input value={row.get('description')} focus={focus} />
+        },
+      },
+      {
+        id: 'partNumbers:0',
+        title: 'Part Number',
+        value: (row, {focus}) => {
+          const mpn = row.getIn(['partNumbers', 0])
+          return (
+            <div>
+              <Input value={mpn.get('manufacturer')} />
+              <Input value={mpn.get('part')} focus={focus} />
+            </div>
+          )
+        },
+      },
+      {
+        id: 'retailers:Digikey',
+        title: 'Digikey',
+        value: retailerRender('Digikey'),
+      },
+      {
+        id: 'retailers:Mouser',
+        title: 'Mouser',
+        value: retailerRender('Mouser'),
+      },
+      {
+        id: 'retailers:RS',
+        title: 'RS',
+        value: retailerRender('RS'),
+      },
+      {
+        id: 'retailers:Farnell',
+        title: 'Farnell',
+        value: retailerRender('Mouser'),
+      },
+    ]
     const props = this.props
+    console.log(this.state.columnWidths)
     return (
       <Grid
-        columnWidthValues={{quantity: 7}}
-        focusOnSingleClick={true}
+        onActiveChanged={({column: newColumn}, {column: prevColumn}) => {
+          console.log({newColumn, prevColumn})
+          if (/^retailers:/.test(prevColumn)) {
+            this.setState(state => ({
+              columnWidths: {...state.columnWidths, [prevColumn]: 7},
+            }))
+          }
+          if (/^retailers:/.test(newColumn)) {
+            this.setState(state => ({
+              columnWidths: {...state.columnWidths, [newColumn]: 20},
+            }))
+          }
+        }}
+        isColumnsResizable={true}
+        columnWidthValues={this.state.columnWidths}
+        focusOnSingleClick={false}
         columns={columns}
         rows={this.props.lines.toArray()}
         getRowKey={i => props.lines.getIn([i, 'id'])}
@@ -167,21 +120,12 @@ function mapStateToProps(state) {
   const lines = state.data.present
     .get('lines')
     .map((line, id) => {
-      const partNumbers = immutable.Map(
-        line.get('partNumbers').map((m, i) => [`partNumbers:${i}`, m])
-      )
-      const retailers = line
-        .get('retailers')
-        .mapEntries(([k, v]) => ['retailers:' + k, v])
-      return line
-        .merge(retailers)
-        .merge(partNumbers)
-        .merge({id})
+      return line.merge({id})
     })
     .toList()
   return {
     lines,
-    order: state.data.present.get('order')
+    order: state.data.present.get('order'),
   }
 }
 

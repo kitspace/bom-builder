60ec19479531d47f67858ef002ddf17ad5a36158
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2019-10-18 19:09:31 +0100

    Add LCSC

--- src/popup.js
+++ src/popup.js
@@ -18,6 +18,8 @@ function getSkuUrl(vendor, sku) {
       return 'https://uk.rs-online.com/web/c/?searchTerm=' + sku
     case 'Rapid':
       return 'https://www.rapidonline.com/Catalogue/Search?Query=' + sku
+    case 'LCSC':
+      return 'https://lcsc.com/search?q=' + sku
     default:
       return '#'
   }
